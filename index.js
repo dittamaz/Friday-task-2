e3fba3e3ebeca389cfe5f2ba38a2c20a8cfc544d
dittamaz <129725385+dittamaz@users.noreply.github.com>
2023-07-14 20:48:32 +0300

    5-7 uzduotys

--- index.js
+++ index.js
@@ -1,7 +1,4 @@
 console.clear();
-//5. Naudojant while ciklą, spausdinti atsitiktinius skaičius nuo 1 iki 10. Paskutinis atspausdintas skaičius turi būti 5. (7 taškai)
-//6. Sukurti masyvą, kurio ilgis būtų nuo 20 iki 30, o reikšmės būtų skaičiai nuo 10 iki 30. Surasti didžiausią masyvo reikšmę, NENAUDOJANT sort() bei Math.max() funkcijų. (7 taškai)
-//7. Sugeneruokite masyvą, kurio reikšmės atsitiktinės raidės A, B, C ir D, o ilgis 100. Suskaičiuokite kiek yra kiekvienos raidės. (7 taškai)
 //8. Parašyti funkciją - lygineSuma. Funkcijos parametrai - du kintamieji. Testų reikalavimai - abu kitamieji turi būti arba skaičiai arba masyvai(negali būti vienas skaičius, kitas masyvas).
 //Jei kintamieji skaičiai, grąžinti skaičių sumą, jei kintamieji masyvai - grąžinti masyvų ilgių sumą. Jei abu kintamieji skaičiai arba masyvai, bet suma nelyginė - grąžinti tekstą, kad suma nelyginė. (10 taškų)
 //9. Parašyti funkciją pirminisSkaicius. Funkcija turi vieną kintamąjį. Turi būti patikrinimas, kad kintamasis yra skaičius. Funkcija turi grąžinti ar pateiktas skaičius yra pirminis( pirminis
@@ -53,4 +50,55 @@ const Skaiciai = () => {
     }
     console.log(Skaiciai());
 
-    
+console.log('-------  5 uzduotis ---------');
+//5. Naudojant while ciklą, spausdinti atsitiktinius skaičius
+// nuo 1 iki 10. Paskutinis atspausdintas skaičius turi būti 5. (7 taškai)
+
+let Skaiciai2 = 0;
+while (Skaiciai2 !== 5) {
+    Skaiciai2 = Math.floor(Math.random() * 10) + 1;
+    console.log(Skaiciai2);
+}
+
+console.log('-------  6 uzduotis ---------');
+//6. Sukurti masyvą, kurio ilgis būtų nuo 20 iki 30, o reikšmės
+// būtų skaičiai nuo 10 iki 30. Surasti didžiausią masyvo reikšmę, NENAUDOJANT sort() bei Math.max() funkcijų. (7 taškai)
+
+let Skaiciai3 = [];
+const arrLength = Math.floor(Math.random() * (30 - 20 + 1) + 20);
+
+console.log(arrLength);
+
+for (let i = 0; i < arrLength; i++) {
+    Skaiciai3.push(Math.floor(Math.random() * (30 - 10 + 1) + 10));
+}
+console.log(Skaiciai3);
+
+console.log('-------  7 uzduotis ---------');
+//7. Sugeneruokite masyvą, kurio reikšmės atsitiktinės raidės A,
+// B, C ir D, o ilgis 100. Suskaičiuokite kiek yra kiekvienos 
+// raidės. (7 taškai)
+
+const array = [];
+const raides = ['A', 'B', 'C', 'D'];
+const arrayLength = 100;
+
+
+for (let i = 0; i < arrayLength; i++) {
+  const randomIndex = Math.floor(Math.random() * raides.length);
+  array.push(raides[randomIndex]);
+}
+
+const kiekis = {
+  A: 0,
+  B: 0,
+  C: 0,
+  D: 0,
+};
+
+for (let i = 0; i < array.length; i++) {
+  kiekis[array[i]]++;
+}
+
+console.log('Raidziu kiekis:', kiekis);
+
